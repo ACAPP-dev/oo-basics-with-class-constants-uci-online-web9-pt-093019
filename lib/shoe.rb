74e03cc8d4bd72b7e891d6e3e2b3e745eeca815c
Andrew Capp <acapp909@gmail.com>
2019-10-31 18:24:14 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,4 @@
+require 'benchmark'
 class Shoe
 
   BRANDS = []
@@ -16,4 +17,10 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+end
+
+Benchmark.bmbm do |x|
+  x.report("detect") {10_000.times{(1..10000)to_a.detect{|number| number == 9999}}}
+
+  x.report("include") {10_000.times{(1..10000)to_a.include?(9999)}
 end
